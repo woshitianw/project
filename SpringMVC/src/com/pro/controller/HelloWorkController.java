5ba5c0ba6e5a3d42c2e392ffd89fbf2dc242c7b0
woshitianw <1117662@qq.com>
2017-05-16 16:04:55 +0800

    v1.0版本提交

---
+++ SpringMVC/src/com/pro/controller/HelloWorkController.java
@@ -0,0 +1,31 @@
+package com.pro.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloWorkController {
+
+	
+	@RequestMapping("/helloword")
+	public String hello() {
+		System.out.println("进入后台控制类");
+		return "success";
+	}
+	
+	@RequestMapping("/testPath/{id}")
+	public String testPath(@PathVariable(value="id") Integer id) {
+		System.out.println("请求ID为："+id);
+		return "success";
+	}
+	
+	@RequestMapping("/requestParams")
+	public String testRequestParam(@RequestParam(value="username") String username,
+			@RequestParam(value="age",required=false,defaultValue="0") Integer age) {
+		
+		System.out.println("用户姓名为："+username+"年龄："+age);
+		return "success";
+	}
+}
